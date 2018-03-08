d2289b0680675783004b846c30e5bd6febadf029
Nick Dower <nicholas.dower@soundcloud.com>
2018-03-08 11:29:03 +0100

    Add stopwatch and live thread counter.

--- app/src/main/java/foo/crasher/CrasherActivity.java
+++ app/src/main/java/foo/crasher/CrasherActivity.java
@@ -6,21 +6,24 @@ import io.fabric.sdk.android.Fabric;
 import io.fabric.sdk.android.InitializationCallback;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.Locale;
 import java.util.concurrent.CountDownLatch;
 
 public class CrasherActivity extends AppCompatActivity {
 
-    private int threadCount = 0;
     private Crasher crasher;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         crasher = new Crasher();
+        handler = new Handler();
         Fabric.with(new Fabric.Builder(this)
                             .kits(new Crashlytics(), new CrashlyticsNdk())
                             .debuggable(true)
@@ -28,7 +31,7 @@ public class CrasherActivity extends AppCompatActivity {
                                 @Override
                                 public void success(Fabric fabric) {
                                     setContentView(R.layout.crasher);
-                                    setThreadCount();
+                                    startThreadCounter();
                                 }
 
                                 @Override
@@ -51,17 +54,15 @@ public class CrasherActivity extends AppCompatActivity {
                 }
             }).start();
         }
-        threadCount += 10;
-        setThreadCount();
     }
 
     public void forceCrashOnMainThread(View view) {
-        disableButtons();
+        disableButtonsAndStartStopwatch();
         crasher.crash();
     }
 
     public void forceCrashOnBackgroundThread(View view) {
-        disableButtons();
+        disableButtonsAndStartStopwatch();
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -71,12 +72,12 @@ public class CrasherActivity extends AppCompatActivity {
     }
 
     public void forceNativeCrashOnMainThread(View view) {
-        disableButtons();
+        disableButtonsAndStartStopwatch();
         crasher.nativeCrash();
     }
 
     public void forceNativeCrashOnBackgroundThread(View view) {
-        disableButtons();
+        disableButtonsAndStartStopwatch();
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -85,16 +86,36 @@ public class CrasherActivity extends AppCompatActivity {
         }).start();
     }
 
-    private void setThreadCount() {
-        TextView threadCountTextView = findViewById(R.id.thread_count);
-        threadCountTextView.setText(getString(R.string.thread_count, threadCount));
-    }
-
-    private void disableButtons() {
+    private void disableButtonsAndStartStopwatch() {
         findViewById(R.id.start_threads).setEnabled(false);
         findViewById(R.id.force_crash_on_background_thread).setEnabled(false);
         findViewById(R.id.force_crash_on_main_thread).setEnabled(false);
         findViewById(R.id.force_native_crash_on_background_thread).setEnabled(false);
         findViewById(R.id.force_native_crash_on_main_thread).setEnabled(false);
+
+        final TextView timerView = findViewById(R.id.stopwatch);
+        final long start = System.nanoTime() / 1000000000;
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                long totalSeconds = (System.nanoTime() / 1000000000) - start;
+                timerView.setText(String.format(Locale.US, "%02d:%02d", totalSeconds / 60, totalSeconds % 60));
+                handler.postDelayed(this, 100);
+            }
+        };
+        handler.post(runnable);
+    }
+
+    private void startThreadCounter() {
+        final TextView threadCountTextView = findViewById(R.id.thread_count);
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                int threadCount = Thread.getAllStackTraces().size();
+                threadCountTextView.setText(getString(R.string.thread_count, threadCount));
+                handler.postDelayed(this, 500);
+            }
+        };
+        handler.post(runnable);
     }
 }
